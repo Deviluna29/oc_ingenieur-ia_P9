e78282e048b2b8e212d36b9e7dc9c457648acdb7
Romain <romain-legoff@hotmail.fr>
2022-08-23 17:10:28 +0200

    Maj azure function

--- azure_function/recommend-article/__init__.py
+++ azure_function/recommend-article/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from math import *
+from heapq import nlargest
 import pickle
 import pandas as pd
 import numpy as np
@@ -14,27 +15,27 @@ try:
     connect_str = os.getenv('AzureWebJobsStorage')
 
     ##### Chargement des fichiers #####
+    # Download blobs
     blob_articles = BlobClient.from_connection_string(conn_str=connect_str, container_name="incontainer", blob_name='articles_embeddings.pickle')
-    blob_clicks = BlobClient.from_connection_string(conn_str=connect_str, container_name="incontainer", blob_name='all_clicks.pickle')
-
-    dl_articles = blob_articles.download_blob().readall()
-    dl_clicks = blob_clicks.download_blob().readall()
+    blob_users = BlobClient.from_connection_string(conn_str=connect_str, container_name="incontainer", blob_name='users.pickle')
+    blob_model = BlobClient.from_connection_string(conn_str=connect_str, container_name="incontainer", blob_name='model_svd.pickle')
 
     # Load to pickle
-    articles_pkl = pickle.loads(dl_articles)
-    articles_df = pd.DataFrame(articles_pkl, columns=["embedding_" + str(i) for i in range(articles_pkl.shape[1])])
+    articles_df = pickle.loads(blob_articles.download_blob().readall())
+    articles_df = pd.DataFrame(articles_df, columns=["embedding_" + str(i) for i in range(articles_df.shape[1])])
 
-    clicks_pkl = pickle.loads(dl_clicks)
-    clicks_df = pd.DataFrame(clicks_pkl)
+    users_df = pickle.loads(blob_users.download_blob().readall())
+
+    model = pickle.loads(blob_model.download_blob().readall())
 
 except Exception as ex:
     print('Exception:')
     print(ex)
 
 # Recommandation Content-Based
-def contentBasedRecommendArticle(articles, clicks, user_id, n=5):
+def contentBasedRecommendArticle(articles, users, user_id, n=5):
 
-    articles_read = clicks[clicks['user_id'] == user_id]['click_article_id'].tolist()
+    articles_read = users['click_article_id'].loc[user_id]
 
     if len(articles_read) == 0:
         return "L'utilisateur n'a lu aucun article"
@@ -59,26 +60,44 @@ def contentBasedRecommendArticle(articles, clicks, user_id, n=5):
 
     return rec
 
+# Recommandation Collaborative Filtering
+def collaborativeFilteringRecommendArticle(articles, users, user_id, n=5):
+
+    index = list(articles.index)
+
+    articles_read = users['click_article_id'].loc[user_id]
+
+    for ele in articles_read:
+        if ele in index:
+            index.remove(ele)
+
+    results = dict()
+
+    for i in index:
+        pred = model['algo'].predict(user_id, i)
+        results[pred.iid] = pred.est
+    
+    return nlargest(n, results, key = results.get)
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    id = req.params.get('id')
-    if not id:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            id = req_body.get('id')
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        pass
+    else:
+        id = req_body.get('id')
+        type = req_body.get('type')
 
-    if id:
+    if id and type and isinstance(id, int) and isinstance(type, str):
 
-        recommended = contentBasedRecommendArticle(articles_df, clicks_df, id)
+        recommended = contentBasedRecommendArticle(articles_df, users_df, id) if type == "cb" else collaborativeFilteringRecommendArticle(articles_df, users_df, id)
 
-        return func.HttpResponse(str(recommended))
+        return func.HttpResponse(str(recommended), status_code=200)
 
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass an id in the query string or in the request body for a personalized response.",
-             status_code=200
+             "Requête invalide.\nDans le body doit figurer sous format json :\n- id (int)\n- type (cb ou cf)",
+             status_code=400
         )
